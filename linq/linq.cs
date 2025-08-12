5e69ad6324d4111b29c770c7d95d2897e815be16
Hui Angeles <149886268+anyweiii@users.noreply.github.com>
2025-08-12 13:55:12 +0800

    Add files via upload

---
+++ linq/linq.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Specify the data source.
+int[] scores = [97, 92, 81, 60];
+
+// Define the query expression.
+IEnumerable<string> scoreQuery =
+    from score in scores
+    where score > 80
+    orderby score descending
+    select $"The score is: {score}";
+
+// Execute the query.
+foreach (string i in scoreQuery)
+{
+    Console.WriteLine(i);
+}
+
+// Output: 97 92 81
