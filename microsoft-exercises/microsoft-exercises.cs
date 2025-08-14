3d82f578390a3beb707759b0ddefe17eb00ecd7b
Hui Angeles <149886268+anyweiii@users.noreply.github.com>
2025-08-14 11:12:04 +0800

    Add files via upload

--- microsoft-exercises/microsoft-exercises.cs
+++ microsoft-exercises/microsoft-exercises.cs
@@ -302,3 +302,61 @@ Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA");
 Console.WriteLine("Nicolas:\t" + nicolasScore + "\tB");
 Console.WriteLine("Zahirah:\t" + zahiraScore + "\tB");
 Console.WriteLine("Jeong:\t\t" + jeongScore + "\tA");
+
+/*  
+    Guided Project v2
+*/
+
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
+
+int gradeA = 4;
+int gradeB = 3;
+
+int course1Grade = gradeA;
+int course2Grade = gradeB;
+int course3Grade = gradeB;
+int course4Grade = gradeB;
+int course5Grade = gradeA;
+
+int totalCreditHours = 0;
+totalCreditHours += course1Credit; // 3
+totalCreditHours += course2Credit; // 3
+totalCreditHours += course3Credit; // 4
+totalCreditHours += course4Credit; // 4
+totalCreditHours += course5Credit; // 3
+
+totalCreditHours = course1Credit + course2Credit + course3Credit + course4Credit + course5Credit; //  3 + 3 + 4 + 4 + 3 = 17
+
+int totalGradePoints = 0;
+totalGradePoints += course1Credit * course1Grade; // 3 * 4
+totalGradePoints += course2Credit * course2Grade; // 3 * 3
+totalGradePoints += course3Credit * course3Grade; // 4 * 3
+totalGradePoints += course4Credit * course4Grade; // 4 * 3
+totalGradePoints += course5Credit * course5Grade; // 3 * 4
+
+decimal gradePointAverage = (decimal)totalGradePoints / totalCreditHours;
+int leadingDigit = (int)gradePointAverage;
+int firstDigit = (int)(gradePointAverage * 10) % 10;
+int secondDigit = (int)(gradePointAverage * 100) % 10;
+
+Console.WriteLine($"\nStudent: {studentName}\n");
+Console.WriteLine($"Course\t\t\tGrade\t\t\tCredit Hours");
+
+Console.WriteLine($"{course1Name}\t\t{course1Grade}\t\t\t{course1Credit}");
+Console.WriteLine($"{course2Name}\t\t{course2Grade}\t\t\t{course2Credit}");
+Console.WriteLine($"{course3Name}\t\t{course3Grade}\t\t\t{course3Credit}");
+Console.WriteLine($"{course4Name}\t{course4Grade}\t\t\t{course4Credit}");
+Console.WriteLine($"{course5Name}\t\t{course5Grade}\t\t\t{course5Credit}");
+
+Console.WriteLine($"\nFinal GPA:\t\t {leadingDigit}.{firstDigit}{secondDigit}");
