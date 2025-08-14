498b5b24d7d484e82206c8da756d7f14d46c96a2
Hui Angeles <149886268+anyweiii@users.noreply.github.com>
2025-08-14 16:38:24 +0800

    Add files via upload

---
+++ microsoft-exercisesv2/microsoft-exercisesv2.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Random dice = new Random();
+// int roll = dice.Next(1, 6);
+// Console.WriteLine(roll);
+
+
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
+/*  
+    Code challenge: Implement a method of the Math class that returns the larger of two numbers
+*/
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue);
