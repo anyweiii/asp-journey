aa8948bb3cd2cc75bf36a8f3086561db4b356682
Hui Angeles <149886268+anyweiii@users.noreply.github.com>
2025-08-15 16:07:33 +0800

    Add files via upload

--- microsoft-exercisesv2/microsoft-exercisesv2.cs
+++ microsoft-exercisesv2/microsoft-exercisesv2.cs
@@ -6,21 +6,120 @@ Console.WriteLine("Hello, World!");
 // Console.WriteLine(roll);
 
 
-Random dice = new Random();
-int roll1 = dice.Next();
-int roll2 = dice.Next(101);
-int roll3 = dice.Next(50, 101);
+// Random dice = new Random();
+// int roll1 = dice.Next();
+// int roll2 = dice.Next(101);
+// int roll3 = dice.Next(50, 101);
 
-Console.WriteLine($"First roll: {roll1}");
-Console.WriteLine($"Second roll: {roll2}");
-Console.WriteLine($"Third roll: {roll3}");
+// Console.WriteLine($"First roll: {roll1}");
+// Console.WriteLine($"Second roll: {roll2}");
+// Console.WriteLine($"Third roll: {roll3}");
 
 /*  
     Code challenge: Implement a method of the Math class that returns the larger of two numbers
 */
 
-int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue = Math.Max(firstValue, secondValue);
 
-Console.WriteLine(largerValue);
+// Console.WriteLine(largerValue);
+
+/*  
+    Create Decision Logic with IF Statements
+*/
+
+Random dice = new Random();
+
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+
+int total = roll1 + roll2 + roll3;
+
+/*  
+    Double Bonus
+*/
+
+/*  
+    Add an if statement to display different messages based on the value of the total variable
+*/
+
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll3 == roll1)) // Compound condition
+{
+    if ((roll1 == roll2) && (roll2 == roll3))
+    {
+        Console.WriteLine("You rolled triples! +6 bonus to total");
+        total += 6;
+    }
+    else
+    {
+        Console.WriteLine("You rolled doubles! +2 bonus to total!");
+        total += 2;
+    }
+}
+
+if (total >= 16)
+{
+    Console.WriteLine("You win a new car!");
+}
+else if (total >= 10)
+{
+    Console.WriteLine("You win a new laptop");
+}
+else if (total == 7)
+{
+    Console.WriteLine("You win a trip for two!");
+}
+else
+{
+    Console.WriteLine("You win a kitten!");
+}
+
+string message = "The quick brown fox jumps over the lazy dog";
+bool result = message.Contains("dog");
+Console.WriteLine(result);
+
+if (message.Contains("fox"))
+{
+    Console.WriteLine("What does the fox say?");
+}
+
+/*  
+    Improve Renewal Rate of Subscriptions
+*/
+
+Random random = new Random();
+int daysUntilExpiration = 5; //random.Next(12);
+int discountPercentage = 0;
+
+// subscription <= 10 (Your subscription will expire soon. Renew now!)
+// subscription <= 5 (Your subscription expires in _ days. Renew now and save 10%)
+// subscription = 1 (Your subscription expires within a day! Renew now and save 20%)
+// subscription > 10 (No display)
+
+if (daysUntilExpiration == 0)
+{
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1)
+{
+    Console.WriteLine("Your subscription expires within a day!");
+    discountPercentage = 20;
+}
+else if (daysUntilExpiration <= 5)
+{
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+    discountPercentage = 10;
+}
+else if (daysUntilExpiration <= 10)
+{
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+if (discountPercentage > 0)
+{
+    Console.WriteLine($"Renew now and save {discountPercentage}%");
+}
