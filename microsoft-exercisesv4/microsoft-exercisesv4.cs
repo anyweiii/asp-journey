9f5b45a8c10dba1bcbc76e8ef9e4efe6e7df7833
Hui Angeles <149886268+anyweiii@users.noreply.github.com>
2025-08-20 17:14:05 +0800

    Add files via upload

---
+++ microsoft-exercisesv4/microsoft-exercisesv4.cs
@@ -0,0 +1,158 @@
+﻿using System.Security.Cryptography;
+
+Console.WriteLine("Discover Integral Type");
+
+// Signed integral types
+Console.WriteLine("Signed integral types:");
+Console.WriteLine($"sbyte   : {sbyte.MinValue} to {sbyte.MaxValue}");
+Console.WriteLine($"short   : {short.MinValue} to {short.MaxValue}");
+Console.WriteLine($"int     : {int.MinValue} to {int.MaxValue}");
+Console.WriteLine($"long    : {long.MinValue} to {long.MaxValue}");
+
+// Unsigned integral types
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
+Console.WriteLine($"byte    : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort  : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint    : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong   : {ulong.MinValue} to {ulong.MaxValue}");
+
+// Floating point types
+Console.WriteLine("");
+Console.WriteLine("Floating point types:");
+Console.WriteLine($"float   : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
+Console.WriteLine($"double  : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
+Console.WriteLine($"decimal : {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+
+// Reference Types
+int[] data = new int[3];
+
+string shortenedString = "Hello World";
+Console.WriteLine(shortenedString);
+
+int val_A = 2;
+int val_B = val_A;
+val_B = 5;
+
+Console.WriteLine("--Value Types--");
+Console.WriteLine($"val_A: {val_A}");
+Console.WriteLine($"val_B: {val_B}");
+
+int[] ref_A = new int[1];
+ref_A[0] = 2;
+int[] ref_B = ref_A;
+ref_B[0] = 5;
+
+Console.WriteLine("--Reference Types--");
+Console.WriteLine($"ref_A[0]: {ref_A[0]}");
+Console.WriteLine($"ref_B[0]: {ref_B[0]}");
+
+// Explore Data Type Casting and Conversion
+
+// int first = 2;
+// string second = "4";
+// string result = first + second;
+// Console.WriteLine(result);
+
+// int myInt = 3;
+// Console.WriteLine($"int: {myInt}");
+
+// decimal myDecimal = myInt;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// decimal myDecimal = 3.14m;
+// Console.WriteLine($"decimal: {myDecimal}");
+
+// int myInt = (int)myDecimal;
+// Console.WriteLine($"int: {myInt}");
+
+decimal myDecimal = 1.23456789m;
+float myFloat = (float)myDecimal;
+
+Console.WriteLine($"Decimal: {myDecimal}");
+Console.WriteLine($"Float  : {myFloat}");
+
+// int first = 5;
+// int second = 7;
+// string message = first.ToString() + second.ToString();
+// Console.WriteLine(message);
+
+// Convert string to an int
+string first = "5";
+string second = "7";
+int sum = int.Parse(first) + int.Parse(second);
+Console.WriteLine(sum);
+
+// string value1 = "5";
+// string value2 = "7";
+// int result = Convert.ToInt32(value1) * Convert.ToInt32(value2);
+// Console.WriteLine(result);
+
+// int value = (int)1.5m; // casting truncates
+// Console.WriteLine(value);
+
+// int value2 = Convert.ToInt32(1.5m); // converting rounds up
+// Console.WriteLine(value2);
+
+// // Examine the TryParse() method
+// string value = "102";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//     Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//     Console.WriteLine("Unable to report the measurement.");
+// }
+
+// string value = "102";
+// int result = 0;
+// if (int.TryParse(value, out result))
+// {
+//    Console.WriteLine($"Measurement: {result}");
+// }
+// else
+// {
+//    Console.WriteLine("Unable to report the measurement.");
+// }
+// Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// string messageResult = "";
+// decimal total = 0m;
+
+// foreach (var value in values)
+// {
+//     decimal number;
+//     if (decimal.TryParse(value, out number))
+//     {
+//         total += number;
+//     }
+//     else
+//     {
+//         messageResult += value;
+//     }
+// }
+
+// Console.WriteLine($"Message: {messageResult}");
+// Console.WriteLine($"Total: {total}");
+
+// Complete a challenge to output math operations as specific number types
+
+int value1 = 11;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+// Your code here to set result1
+// Hint: You need to round the result to nearest integer (don't just truncate)
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+// Your code here to set result2
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+// Your code here to set result3
+float result3 = value3 / (float)value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
