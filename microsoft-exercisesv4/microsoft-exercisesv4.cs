82f34db68aebfe40bcae5ff73a958dd27002eef9
Hui Angeles <149886268+anyweiii@users.noreply.github.com>
2025-08-23 21:12:29 +0800

    Add files via upload

--- microsoft-exercisesv4/microsoft-exercisesv4.cs
+++ microsoft-exercisesv4/microsoft-exercisesv4.cs
@@ -244,3 +244,19 @@ for (int i = 0; i < words.Length; i++)
 }
 string reversedSentence = string.Join(" ", words);
 Console.WriteLine(reversedSentence);
+
+string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
+string[] items = orderStream.Split(',');
+Array.Sort(items);
+
+foreach (var item in items)
+{
+    if (item.Length == 4)
+    {
+        Console.WriteLine(item);
+    }
+    else
+    {
+        Console.WriteLine(item + "\t- Error");
+    }
+}
