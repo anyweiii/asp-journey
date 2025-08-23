d124ebacd9e5453701e9a22c035d6c3a4bb51dfd
Hui Angeles <149886268+anyweiii@users.noreply.github.com>
2025-08-23 20:53:32 +0800

    Add files via upload

--- microsoft-exercisesv4/microsoft-exercisesv4.cs
+++ microsoft-exercisesv4/microsoft-exercisesv4.cs
@@ -156,3 +156,91 @@ Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {r
 // Your code here to set result3
 float result3 = value3 / (float)value1;
 Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
+
+// // Discover Sort and Reverse
+// string[] pallets = ["B14", "A11", "B12", "A13"];
+// Console.WriteLine("Sorted....");
+// Array.Sort(pallets);
+
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine("");
+// Console.WriteLine("Reversed...");
+// Array.Reverse(pallets);
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// // Explore Clear and Resize
+// // Clear (pallets - this is the list, 0 is the length, 2 is the count)
+// Array.Clear(pallets, 0, 2);
+// // dont get confused. this clears only not deletes
+// Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+// foreach (var pallet in pallets)
+// {
+//     Console.WriteLine($"-- {pallet}");
+// }
+
+// Console.WriteLine($"Before: {pallets[0]}");
+// Array.Clear(pallets, 0, 2);
+// Console.WriteLine($"After: {pallets[0]}");
+
+string[] pallets = [ "B14", "A11", "B12", "A13" ];
+Console.WriteLine("");
+
+Array.Clear(pallets, 0, 2);
+Console.WriteLine($"Clearing 2 ... count: {pallets.Length}");
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 6);
+Console.WriteLine($"Resizing 6 ... count: {pallets.Length}");
+
+pallets[4] = "C01";
+pallets[5] = "C02";
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+Console.WriteLine("");
+Array.Resize(ref pallets, 3);
+Console.WriteLine($"Resizing 3 ... count: {pallets.Length}");
+
+foreach (var pallet in pallets)
+{
+    Console.WriteLine($"-- {pallet}");
+}
+
+// string value = "abc123";
+// char[] valueArray = value.ToCharArray();
+// Array.Reverse(valueArray);
+// // string result = new string(valueArray);
+// string result = String.Join(",", valueArray);
+// Console.WriteLine(result);
+
+// string[] items = result.Split(',');
+// foreach (string item in items)
+// {
+//     Console.WriteLine(item);
+// }
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+string[] words = pangram.Split(' ');
+for (int i = 0; i < words.Length; i++)
+{
+    char[] wordArray = words[i].ToCharArray();
+    Array.Reverse(wordArray);
+    words[i] = new string(wordArray);
+}
+string reversedSentence = string.Join(" ", words);
+Console.WriteLine(reversedSentence);
